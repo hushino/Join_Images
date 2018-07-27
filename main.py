bf006f1a627a951486ff6ad3ebe1cceaf373ccd0
Hushino <frostshock@outlook.com>
2018-07-26 23:55:02 -0300

    commit

--- main.py
+++ main.py
@@ -16,8 +16,25 @@ final = Image.open("1.png")
 
 final.paste(gato3, area1, gato3)
 png_info1 = final.info
+
 final.save('1.png', **png_info1)
 
+im = Image.open("1.png")
+
+
+# def redorblack(im):
+#     newimdata = []
+#     redcolor = (0, 0, 0)
+#     blackcolor = (0, 0, 0)
+#     for color in im.getdata():
+#         if color == redcolor:
+#             newimdata.append(redcolor)
+#         else:
+#             newimdata.append(blackcolor)
+#     newim = Image.new(im.mode, im.size)
+#     newim.putdata(newimdata)
+#     return newim
+
+
+# redorblack(im).save("1.png")
 # sister.show()
-
-
